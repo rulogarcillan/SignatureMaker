49550eaaa07c4930c4922e5ac4a1144ed1361127
rulogarcillan <temporal>
2015-03-31 17:30:36 +0200

    Continue..

--- app/src/main/java/com/signaturemaker/app/Constantes/PreferencesCons.java
+++ app/src/main/java/com/signaturemaker/app/Constantes/PreferencesCons.java
@@ -1,5 +1,7 @@
 package com.signaturemaker.app.Constantes;
 
+import android.graphics.Color;
+
 /**
  * Created by rulo on 19/03/15.
  */
@@ -7,4 +9,7 @@ public final class PreferencesCons {
 
     public static final String RUTAFILE = "RUTAFILE";
     public static final String RUTABACKUP = "RUTABACKUP";
+
+    public static int colorTrazo = Color.parseColor("#200BB2");
+    public static int strokeTrazo = 3;
 }
--- app/src/main/java/com/signaturemaker/app/Nucleo/MainActivity.java
+++ app/src/main/java/com/signaturemaker/app/Nucleo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.signaturemaker.app.Nucleo;
 
+import android.content.res.Configuration;
 import android.gesture.GestureOverlayView;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -8,17 +9,23 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
-import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.daimajia.androidanimations.library.Techniques;
 import com.daimajia.androidanimations.library.YoYo;
 import com.getbase.floatingactionbutton.FloatingActionButton;
 import com.getbase.floatingactionbutton.FloatingActionsMenu;
+import com.larswerkman.holocolorpicker.ColorPicker;
+import com.larswerkman.holocolorpicker.SVBar;
 import com.nineoldandroids.animation.Animator;
 import com.signaturemaker.app.R;
 
+import org.adw.library.widgets.discreteseekbar.DiscreteSeekBar;
+
 import de.cketti.library.changelog.ChangeLog;
+
+import static com.signaturemaker.app.Constantes.PreferencesCons.colorTrazo;
+import static com.signaturemaker.app.Constantes.PreferencesCons.strokeTrazo;
 
 
 public class MainActivity extends BaseActivity {
@@ -42,19 +49,23 @@ public class MainActivity extends BaseActivity {
     }
 
 
-
     /**
      * A placeholder fragment containing a simple view.
      */
     public static class PlaceholderFragment extends Fragment {
 
-        FloatingActionsMenu fab1, fab2;
-        FloatingActionButton blimpiar, btrazo;
-        GestureOverlayView gestos ;
-        TextView txtMnsVacio;
-        private SeekBar trazo;
+        private FloatingActionsMenu fab1, fab2;
+        private FloatingActionButton blimpiar, btrazo, bcolor;
+        private GestureOverlayView gestos;
+        private TextView txtMnsVacio;
+        private DiscreteSeekBar trazo;
         private LinearLayout layoutSeek;
-        View rootView;
+        private LinearLayout layoutColor;
+        private View rootView;
+        private ColorPicker picker;
+        private SVBar svBar;
+
+
         public PlaceholderFragment() {
         }
 
@@ -66,23 +77,26 @@ public class MainActivity extends BaseActivity {
             fab2 = (FloatingActionsMenu) rootView.findViewById(R.id.Fab2);
             blimpiar = (FloatingActionButton) rootView.findViewById(R.id.blimpiar);
             btrazo = (FloatingActionButton) rootView.findViewById(R.id.btrazo);
+            bcolor = (FloatingActionButton) rootView.findViewById(R.id.bcolor);
             gestos = (GestureOverlayView) rootView.findViewById(R.id.signaturePad);
             txtMnsVacio = (TextView) rootView.findViewById(R.id.txtMnsVacio);
-            trazo = (SeekBar) rootView.findViewById(R.id.trazo);
+            trazo = (DiscreteSeekBar) rootView.findViewById(R.id.trazo);
             layoutSeek = (LinearLayout) rootView.findViewById(R.id.layoutSeek);
+            layoutColor = (LinearLayout) rootView.findViewById(R.id.layoutColor);
+            picker = (ColorPicker) rootView.findViewById(R.id.picker);
+            svBar = (SVBar) rootView.findViewById(R.id.svbar);
 
+            picker.addSVBar(svBar);
 
+            cargarPref();
 
-
-
-
-            gestos.setGestureStrokeWidth(3);
 
             gestos.addOnGestureListener(new GestureOverlayView.OnGestureListener() {
                 @Override
                 public void onGestureStarted(GestureOverlayView overlay, MotionEvent event) {
                     txtMnsVacio.setVisibility(View.INVISIBLE);
                     layoutSeekInv();
+                    layoutColInv();
                 }
 
                 @Override
@@ -106,11 +120,8 @@ public class MainActivity extends BaseActivity {
                 @Override
                 public void onClick(View v) {
 
-                    gestos.cancelClearAnimation();
-                    gestos.clear(true);
-                    gestos.invalidate();
                     colapsarBotones();
-                    txtMnsVacio.setVisibility(View.VISIBLE);
+                    clearScreen();
                 }
             });
 
@@ -118,16 +129,40 @@ public class MainActivity extends BaseActivity {
             btrazo.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    if (layoutSeek.getVisibility() == View.VISIBLE){
+                    if (layoutSeek.getVisibility() == View.VISIBLE) {
                         layoutSeekInv();
 
-                    }else{
+                    } else {
+
+                        if (layoutColor.getVisibility() == View.VISIBLE)
+                            layoutColInv();
+
                         layoutSeekVi();
 
+                    }
                 }
-            }});
+            });
 
-            fab1.setOnFloatingActionsMenuUpdateListener( new FloatingActionsMenu.OnFloatingActionsMenuUpdateListener() {
+
+            bcolor.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (layoutColor.getVisibility() == View.VISIBLE) {
+                        layoutColInv();
+
+                    } else {
+
+                        if (layoutSeek.getVisibility() == View.VISIBLE)
+                            layoutSeekInv();
+
+                        layoutColVi();
+
+                    }
+                }
+            });
+
+
+            fab1.setOnFloatingActionsMenuUpdateListener(new FloatingActionsMenu.OnFloatingActionsMenuUpdateListener() {
                 @Override
                 public void onMenuExpanded() {
 
@@ -142,71 +177,77 @@ public class MainActivity extends BaseActivity {
             });
 
             // cambio de trazo
-            trazo.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-
+            trazo.setOnProgressChangeListener(new DiscreteSeekBar.OnProgressChangeListener() {
                 @Override
-                public void onStopTrackingTouch(SeekBar seekBar) {
-                    // TODO Auto-generated method stub
-                }
-
-                @Override
-                public void onStartTrackingTouch(SeekBar seekBar) {
-                    // TODO Auto-generated method stub
-
-                }
-
-                @Override
-                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                    // TODO Auto-generated method stub
-
-                    gestos.setGestureStrokeWidth(progress);
+                public void onProgressChanged(DiscreteSeekBar seekBar, int value, boolean fromUser) {
+                    gestos.setGestureStrokeWidth(value);
                     gestos.invalidate();
+                }
+
+                @Override
+                public void onStartTrackingTouch(DiscreteSeekBar seekBar) {
+
+                }
+
+                @Override
+                public void onStopTrackingTouch(DiscreteSeekBar seekBar) {
 
                 }
             });
 
+            picker.setOnColorChangedListener(new ColorPicker.OnColorChangedListener() {
+                @Override
+                public void onColorChanged(int i) {
 
+                    colorTrazo = i;
+
+                }
+            });
 
             return rootView;
         }
 
-        public void colapsarBotones(){
+        public void colapsarBotones() {
             fab2.collapse();
             fab1.collapse();
             layoutSeekInv();
+            layoutColInv();
         }
 
-        public void expandirBotones(){
+        public void expandirBotones() {
             fab2.expand();
             fab1.expand();
         }
 
-        private boolean animacion=false;
-        public void layoutSeekInv(){
+        private boolean animacionSeek = false;
+        private boolean animacionClear = false;
+        private boolean animacionCol = false;
 
-            if (!animacion) {
+        public void layoutSeekInv() {
+
+            if (!animacionSeek) {
                 YoYo.with(Techniques.TakingOff)
                         .duration(400).withListener(new Animator.AnimatorListener() {
                     @Override
                     public void onAnimationStart(Animator animation) {
 
-                        animacion = true;
+                        animacionSeek = true;
                     }
 
                     @Override
                     public void onAnimationEnd(Animator animation) {
                         layoutSeek.setVisibility(View.INVISIBLE);
-                        animacion=false;
+                        animacionSeek = false;
                     }
 
                     @Override
                     public void onAnimationCancel(Animator animation) {
-                        animacion=false;
+                        animacionSeek = false;
                     }
 
                     @Override
                     public void onAnimationRepeat(Animator animation) {
-
+                        animacionSeek = false;
                     }
                 })
                         .playOn(rootView.findViewById(R.id.layoutSeek));
@@ -214,12 +255,125 @@ public class MainActivity extends BaseActivity {
 
         }
 
-        public void layoutSeekVi(){
+        public void layoutSeekVi() {
             layoutSeek.setVisibility(View.VISIBLE);
             YoYo.with(Techniques.DropOut)
                     .duration(700)
                     .playOn(rootView.findViewById(R.id.layoutSeek));
 
         }
+
+
+        public void layoutColInv() {
+
+            if (!animacionCol) {
+
+                picker.setColor(colorTrazo);
+                picker.setOldCenterColor(colorTrazo);
+                bcolor.setColorNormal(colorTrazo);
+                gestos.setGestureColor(colorTrazo);
+                gestos.setUncertainGestureColor(colorTrazo);
+                gestos.removeOnGesturePerformedListener(null);
+                gestos.addOnGesturePerformedListener(null);
+                gestos.invalidate();
+
+                YoYo.with(Techniques.TakingOff)
+                        .duration(400).withListener(new Animator.AnimatorListener() {
+                    @Override
+                    public void onAnimationStart(Animator animation) {
+
+                        animacionCol = true;
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        layoutColor.setVisibility(View.INVISIBLE);
+                        animacionCol = false;
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+                        animacionCol = false;
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animation) {
+
+                    }
+                })
+                        .playOn(rootView.findViewById(R.id.layoutColor));
+            }
+
+        }
+
+        public void layoutColVi() {
+            layoutColor.setVisibility(View.VISIBLE);
+
+            YoYo.with(Techniques.DropOut)
+                    .duration(700)
+                    .playOn(rootView.findViewById(R.id.layoutColor));
+
+        }
+
+        public void cargarPref() {
+            gestos.setGestureStrokeWidth(strokeTrazo);
+            trazo.setProgress(strokeTrazo);
+            picker.setColor(colorTrazo);
+            picker.setOldCenterColor(colorTrazo);
+            bcolor.setColorNormal(colorTrazo);
+            gestos.setGestureColor(colorTrazo);
+            gestos.setUncertainGestureColor(colorTrazo);
+
+            gestos.removeOnGesturePerformedListener(null);
+            gestos.addOnGesturePerformedListener(null);
+            gestos.invalidate();
+
+        }
+
+
+        public void clearScreen() {
+
+            if (!animacionClear) {
+                YoYo.with(Techniques.TakingOff)
+                        .duration(400).withListener(new Animator.AnimatorListener() {
+                    @Override
+                    public void onAnimationStart(Animator animation) {
+
+                        animacionClear = true;
+                        txtMnsVacio.setVisibility(View.VISIBLE);
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        gestos.cancelClearAnimation();
+                        gestos.clear(true);
+                        gestos.invalidate();
+
+                        YoYo.with(Techniques.DropOut).duration(1).playOn(rootView.findViewById(R.id.signaturePad));
+                        animacionClear = false;
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+                        animacionClear = false;
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animation) {
+                        animacionClear = false;
+                    }
+                })
+                        .playOn(rootView.findViewById(R.id.signaturePad));
+            }
+        }
+
+        @Override
+        public void onConfigurationChanged(Configuration newConfig) {
+            super.onConfigurationChanged(newConfig);
+
+            clearScreen();
+        }
+
+
     }
 }
