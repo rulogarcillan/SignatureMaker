8f045c2ceb2a16213338f049432a80d878972c6f
DESKTOP-FIKT7AI\Rulo <raulrcs@gmail.com>
2018-06-18 23:34:54 +0200

    Rewrite code

---
+++ app/src/main/java/com/signaturemaker/app/activities/BaseActivity.java
@@ -0,0 +1,113 @@
+/*
+ __ _                   _                                 _
+/ _(_) __ _ _ __   __ _| |_ _   _ _ __ ___    /\/\   __ _| | _____ _ __
+\ \| |/ _` | '_ \ / _` | __| | | | '__/ _ \  /    \ / _` | |/ / _ \ '__|
+_\ \ | (_| | | | | (_| | |_| |_| | | |  __/ / /\/\ \ (_| |   <  __/ |
+\__/_|\__, |_| |_|\__,_|\__|\__,_|_|  \___| \/    \/\__,_|_|\_\___|_|
+      |___/
+
+Copyright (C) 2018  Raúl Rodríguez Concepción
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
+
+package com.signaturemaker.app.activities;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.mikepenz.aboutlibraries.Libs;
+import com.mikepenz.aboutlibraries.LibsBuilder;
+import com.signaturemaker.app.R;
+import com.signaturemaker.app.comun.Constants;
+
+
+import static com.signaturemaker.app.comun.Utils.getAppTimeStamp;
+
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+
+        switch (menuItem.getItemId()) {
+
+            case R.id.settings:
+                break;
+            case R.id.action_settings:
+                break;
+            case R.id.puntuar:
+                startRate();
+                break;
+            case R.id.more:
+                startMoreApps();
+                break;
+            case R.id.changelog:
+                //new LanzaChangelog(BaseActivity.this).getFullLogDialog().show();
+                break;
+            case R.id.license:
+                startLicense();
+                break;
+            default:
+                onBackPressed();
+        }
+        return true;
+    }
+
+    /**
+     * Star Activity to rate it
+     */
+    private void startRate() {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("market://details?id=com.wepica.sounddreams"));
+        startActivity(intent);
+    }
+
+    /**
+     * Start activity my others play store apps
+     */
+    private void startMoreApps() {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://search?q=pub:Raúl R."));
+        startActivity(intent);
+    }
+
+
+
+    private void startLicense() {
+
+        String fechaCompilacion = getAppTimeStamp(getApplicationContext());
+        new LibsBuilder()
+                .withFields(R.string.class.getFields())
+                .withAutoDetect(true)
+                .withVersionShown(true)
+                .withLicenseShown(true)
+                .withAboutIconShown(true)
+                .withAboutVersionShown(true)
+                .withAboutDescription(getResources().getString(R.string.app_written) + "&emsp;<a href='https://www.linkedin.com/in/raul-rodriguez-concepcion/'>Linkedin</a>" + "<br/><i>" + fechaCompilacion + "</i></b>" + "<br/><br/><b>License GNU GPL V3.0</b><br/><br/><a href=\"https://github.com/rulogarcillan/Sound-Dreams\">Project in Github</a>")
+                .withAboutAppName(getString(R.string.app_name))
+                .withActivityTitle(getResources().getString(R.string.title_license))
+                .withActivityStyle(Libs.ActivityStyle.LIGHT_DARK_TOOLBAR)
+                .start(this);
+    }
+}
+
+
---
+++ app/src/main/java/com/signaturemaker/app/comun/Utils.java
@@ -0,0 +1,57 @@
+/*
+ __ _                   _                                 _
+/ _(_) __ _ _ __   __ _| |_ _   _ _ __ ___    /\/\   __ _| | _____ _ __
+\ \| |/ _` | '_ \ / _` | __| | | | '__/ _ \  /    \ / _` | |/ / _ \ '__|
+_\ \ | (_| | | | | (_| | |_| |_| | | |  __/ / /\/\ \ (_| |   <  __/ |
+\__/_|\__, |_| |_|\__,_|\__|\__,_|_|  \___| \/    \/\__,_|_|\_\___|_|
+      |___/
+
+Copyright (C) 2018  Raúl Rodríguez Concepción
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
+package com.signaturemaker.app.comun;
+
+
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.util.Log;
+
+import java.io.File;
+import java.text.DateFormat;
+
+public final class Utils {
+
+    private Utils() {
+    }
+
+    /**
+     * Get last time compilation app
+     *
+     * @param context
+     * @return
+     */
+    public static String getAppTimeStamp(Context context) {
+        String timeStamp = "";
+        try {
+            ApplicationInfo appInfo = context.getPackageManager().getApplicationInfo(context.getPackageName(), 0);
+            String appFile = appInfo.sourceDir;
+            long time = new File(appFile).lastModified();
+            DateFormat formatter = DateFormat.getDateTimeInstance();
+            timeStamp = formatter.format(time);
+        } catch (Exception e) {
+            Log.e(Constants.TAG, e.getMessage());
+        }
+        return timeStamp;
+    }
+}
