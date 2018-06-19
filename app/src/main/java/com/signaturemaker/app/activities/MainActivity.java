b97294bbd1009fcc9a394057e7beb88a33a4f6d9
DESKTOP-FIKT7AI\Rulo <raulrcs@gmail.com>
2018-06-19 21:16:13 +0200

    adding floating action buttons

--- app/src/main/java/com/signaturemaker/app/activities/MainActivity.java
+++ app/src/main/java/com/signaturemaker/app/activities/MainActivity.java
@@ -40,14 +40,6 @@ public class MainActivity extends BaseActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
