a096d5a6789e0616ff1e6130c2c546e548ec06c9
Rulogarcillan <raulrcs@gmail.com>
2018-09-11 16:52:29 +0200

    update libraries and fix bug back in setting

--- app/src/main/java/com/signaturemaker/app/activities/SettingsActivity.java
+++ app/src/main/java/com/signaturemaker/app/activities/SettingsActivity.java
@@ -229,7 +229,8 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
         public boolean onOptionsItemSelected(MenuItem item) {
             int id = item.getItemId();
             if (id == android.R.id.home) {
-                startActivity(new Intent(getActivity(), SettingsActivity.class));
+               // startActivity(new Intent(getActivity(), SettingsActivity.class));
+                getActivity().onBackPressed();
                 return true;
             }
             return super.onOptionsItemSelected(item);
